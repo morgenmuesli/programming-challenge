561279c40ce595ce05ee41436ee5b7b6363671f7
Christoph Meyer <christoph-1.meyer@uni-ulm.de>
2023-06-21 17:06:34 +0200

    refactor:
    refactor App java and add some cmd interface

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -2,10 +2,12 @@ package de.exxcellent.challenge;
 
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
- * design. Read: create your own classes and packages as appropriate.
+ * The entry class for your solution. This class is only aimed as starting point and not intended as
+ * baseline for your software design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
@@ -13,39 +15,101 @@ import java.io.IOException;
 
 public final class App {
 
-    public static final String WEATHER_PATH = "de/exxcellent/challenge/weather.csv";
-    public static final String FOOTBALL_PATH = "de/exxcellent/challenge/football.csv";
+
+  /**
+   * Argument parser for parsing inputs using key value parse like this: args= ["--football",
+   * "football.csv", "--weather", "weather.csv"] returns into a HashMap containing {"football":
+   * "football.csv", "weather", "weather.csv"}
+   *
+   * @param args a string array containing key value pairs
+   * @return a HashMap with the key value pairs
+   */
+
+  public static Map<String, String> parseArguments(String[] args) {
+    Map<String, String> parsedArguments = new HashMap<>();
+    String currentKey = null;
+
+    for (int i = 0; i < args.length; i++) {
+      String arg = args[i].trim();
+
+      if (arg.startsWith("--")) {
+        // Remove the leading "--" to get the argument key
+        currentKey = arg.substring(2).trim();
+      } else if (currentKey != null) {
+        // Store the argument value with the current key
+        parsedArguments.put(currentKey, arg);
+        currentKey = null; // Reset the key
+      }
+    }
+
+    return parsedArguments;
+  }
 
     /**
-     * This is the main entry method of your program.
-     * @param args The CLI arguments passed
+     * returns the day with the smallest distance between min and max temperatur
+     * @param path path for the csv file
+     * @return the day with the smallest difference
      */
-    public static void main(String... args) {
-        try {
-            ICSVReader weatherReader = new CsvReader(WEATHER_PATH);
-            ICSVReader goalReader = new CsvReader(FOOTBALL_PATH);
+  public static String getSmallestDifferenceDay(String path) {
+    IReader reader = null;
+    try {
+      reader = new CsvReader(path);
+      String indexName = "Day";
+      String[] relevantColumns = {"MxT", "MnT"};
 
+      return DatasetAnalyzer.getValueWithSmallestDistance(
+          reader, indexName,
+          relevantColumns);
 
-            String dayWithSmallestTempSpread = DatasetAnalyzer.getValueWithSmallestDistance(
-                weatherReader, "Day",
-                new String[]{"MxT", "MnT"});
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-            System.out.printf("Day with smallest temperature spread : %s%n",
-                dayWithSmallestTempSpread);
+  public static String getSmallestDifferenceTeam(String path) {
+    IReader reader = null;
+    try {
+      reader = new CsvReader(path);
+      String indexName = "Team";
+      String[] relevantColumns = {"Goals", "Goals Allowed"};
 
-            String teamWithSmallestGoalSpread = DatasetAnalyzer.getValueWithSmallestDistance(
-                goalReader, "Team",
-                new String[]{"Goals", "Goals Allowed"});
-            System.out.printf("Team with smallest goal spread       : %s%n",
-                teamWithSmallestGoalSpread);
+      return DatasetAnalyzer.getValueWithSmallestDistance(
+          reader, indexName,
+          relevantColumns);
 
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  /**
+   * This is the main entry method of your program.
+   *
+   * @param args The CLI arguments passed
+   */
+  public static void main(String... args) {
+
+    // Parse the arguments
+    Map<String, String> parsedArguments = parseArguments(args);
+    // Access the parsed arguments
+    final String footballPath = parsedArguments.get("football");
+    final String weatherPath = parsedArguments.get("weather");
+
+    if (weatherPath != null) {
+      String dayWithSmallestTempSpread = getSmallestDifferenceDay(weatherPath);
+      System.out.printf("Day with smallest temperature spread : %s%n",
+          dayWithSmallestTempSpread);
+    }
+
+    if (footballPath != null) {
+      String teamWithSmallestGoalSpread = getSmallestDifferenceTeam(footballPath);
+      System.out.printf("Team with smallest goal spread       : %s%n",
+          teamWithSmallestGoalSpread);
     }
 
 
-            // Your day analysis function call …
+  }
+
+  // Your day analysis function call …
 
 }
